d69b94a7fa4fff057e19395060bf85d9371efacc
fabricepaka <fabricepaka10@gmail.com>
2026-06-22 02:52:13 +0200

    Ajout d'un code special pour le calcul de n'importe quelle Integrale

---
+++ Programmation/Methode_Monte_carlo/PI/Calcul_pi_complet_avancee.py
@@ -0,0 +1,65 @@
+import numpy as np 
+from vpython import *
+def f(x):
+    return np.sqrt(1-x**2)
+
+def Integration_MC(a:float, b:float, N: int, f: callable)-> float:
+    """Estimer la valeur d'une integrale en dimension 1
+    
+    Args
+    ----
+    a: float    borne inferieur de l'integrale
+    b: float    borne superieur de l'integrale
+    N: int      Nombre total de valeur à tirer au hasard suivant la loi uniforme
+    f: callable  fonctions à integrer 
+
+    Retourne
+    --------
+    float:  l'approche de l'integrale de f(x) sur [a,b] par la méthode de Monte Carlo
+    l'affichage de l'animation du tirage des points dans le calcul de l'integrale de f(x) sur [a,b] 
+    l'estimation de l'erreur sur le calcul de l'integrale
+
+    """
+
+
+    # Initialisation des variables et des graphes pour l'animation
+    g1 = graph(title="Représentation graphique de l'ensemble des points", xtitle='x', ytitle='y')
+    gd1 = gdots(graph = g1, color = color.red)
+    gd2 = gdots(graph = g1, color = color.blue)
+    i = 0
+    somme_fx_i = 0
+    somme_fx_i2 = 0
+    A = b-a
+
+    while i < N:
+        
+        # Choix d'un point aleatoire dans l'intervalle [a,b]
+        x_i = np.random.random()
+        f_xi = f(x_i)
+        somme_fx_i +=  f_xi                      # sommation des f(x_i)
+        somme_fx_i2 +=  f_xi*f_xi                # sommation des f(x_i)**2
+
+        # Partie graphique de l'animation
+        y_i = np.random.random()
+        if y_i > f_xi:
+
+            gd1.plot(x_i,f_xi)                  # Affichage du point interieur
+        else:
+            gd2.plot(x_i,y_i)                    # Affichage du point exterieur
+    
+    
+        i += 1
+
+    # estimation de l'integrale et de l'erreur sur le calcul de l'integrale
+    somme_fx_i /= float(N)                                       # <f(x)> 
+    somme_fx_i2 /= float(N)                                      #<f(x)**2>
+    integrale = A*somme_fx_i                                     # calcul de l'aire sous la courbe de f(x) sur [a,b]                             
+    var_fx_i = somme_fx_i2 - somme_fx_i**2                       #  variance = <f(x)**2> - <f(x)>**2
+    Integration_MC.erreur = np.sqrt(var_fx_i/float(N))
+    return integrale
+
+
+if __name__ == "__main__" :
+   for N in [1e3,1e4, 1e5]:
+       print( f" Pour N = {N}\n Pi = {4*Integration_MC(0,1,N, f)} +/- {Integration_MC.erreur}")
+
